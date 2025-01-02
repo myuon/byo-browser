bd301d7dccbaecf1c7af14f593344fb052429b6b
myuon <ioi.joi.koi.loi@gmail.com>
2025-01-02 12:02:08 +0900

    feat: update rendering

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::num::NonZeroU32;
 
 use helper::default_typeface;
-use skia_safe::{Font, Paint, TextBlob};
+use skia_safe::{Font, Paint, Rect, TextBlob};
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
@@ -48,17 +48,36 @@ impl ApplicationHandler for App {
                 let mut raster_surface =
                     skia_safe::surfaces::raster_n32_premul((width as i32, height as i32)).unwrap();
                 let canvas = raster_surface.canvas();
-                let paint = Paint::default();
+                let mut paint = Paint::default();
 
                 canvas.clear(0xFFFFFFFF);
 
+                paint.set_argb(0xFF, 0x99, 0x99, 0x99);
+                canvas.draw_rect(Rect::new(0.0, 0.0, width as f32, 50.0), &paint);
+
+                paint.set_argb(0xFF, 0xDD, 0xDD, 0xDD);
+                canvas.draw_rect(Rect::new(0.0, 50.0, width as f32, 120.0), &paint);
+
+                paint.set_argb(0xFF, 0xFF, 0xFF, 0xFF);
+                canvas.draw_rect(Rect::new(20.0, 60.0, 1000.0, 110.0), &paint);
+
                 let text = TextBlob::from_str(
-                    "Hello, Skia!",
+                    "http://example.com",
                     &Font::from_typeface(default_typeface(), 36.0),
                 )
                 .unwrap();
 
-                canvas.draw_text_blob(&text, (50, 45), &paint);
+                paint.set_argb(0xFF, 0x00, 0x00, 0x00);
+                canvas.draw_text_blob(&text, (25, 60 + 36), &paint);
+
+                let text = TextBlob::from_str(
+                    "Hello, from GUI!",
+                    &Font::from_typeface(default_typeface(), 36.0),
+                )
+                .unwrap();
+
+                paint.set_argb(0xFF, 0x00, 0x00, 0x00);
+                canvas.draw_text_blob(&text, (20, 140 + 36), &paint);
 
                 let pixdata = canvas.peek_pixels().unwrap();
                 let pixdata = pixdata.bytes().unwrap();
